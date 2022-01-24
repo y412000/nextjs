26a9a9af850e8a403ed861f9bcabc577ccb73dc7
jane <y412000@hotmail.com>
2022-01-24 20:57:23 +0800

    課程內推薦 css 套件設置

---
+++ nextjs-blog/components/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: [
+    // Use *.tsx if using TypeScript
+    "./pages/**/*.js",
+    "./components/**/*.js",
+    "./pages/**/*.tsx",
+    "./components/**/*.tsx",
+  ],
+  // ...
+};
